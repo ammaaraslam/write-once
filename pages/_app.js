f0566b8ae770a1f2252b9c978e4ae84aeafcde46
Ammaar Aslam <ammaaraslam7@gmail.com>
2022-10-15 23:36:49 +0530

    create basic logic to signup users with a modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,22 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { NhostNextProvider, NhostClient } from "@nhost/nextjs";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <ThemeProvider
+        forcedTheme={Component.theme || undefined}
+        attribute="class"
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </NhostNextProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
+import SignUpModal from "../components/SignUpModal";
 
 export default function Home() {
+  const [signUp, setSignUp] = useState(false);
   return (
     <Layout title="WriteOnce" onlyMeta={true}>
-      <div>hello</div>
+      <div className="w-full h-full min-h-screen z-10">
+        hello <button onClick={() => setSignUp(true)}>click</button>
+      </div>
+      <SignUpModal opened={signUp} onClose={() => setSignUp(false)} />
     </Layout>
   );
 }
